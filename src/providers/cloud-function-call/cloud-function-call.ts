ee4a484895f0f907ab21263ca37f2a966a1177f4
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2019-04-17 17:48:19 +0530

    modify service file

--- src/providers/cloud-function-call/cloud-function-call.ts
+++ src/providers/cloud-function-call/cloud-function-call.ts
@@ -10,10 +10,6 @@ export class CloudFunctionCallProvider {
 
   callHelloFunction() {
     return new Promise<any>((resolve, reject) => {
-      var headers = new Headers();
-      // headers.append('Content-Type', 'application/x-www-form-urlencoded');
-      // headers.append('Access-Control-Allow-Origin' , '*')
-      let options = new RequestOptions({ headers: headers });
       this.http.get('CLOUD_FUNCTION_URL').subscribe((res) => {
         resolve(res);
       }, (err) => {
